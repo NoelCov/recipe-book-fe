72c130bd2cfd9175e41985719bea5ef0a30c72d8
Noel <no3lcodes@gmail.com>
2024-03-30 23:59:22 -0500

    Made component have react state

--- components/input/input.tsx
+++ components/input/input.tsx
@@ -1,19 +1,23 @@
-import React, { HTMLInputTypeAttribute } from 'react'
+'use client'
 
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useState } from "react";
 
 interface input {
     label: string,
     placeholder: string,
-    name: string
+    name: string,
+    initialValue: string
 }
 
-const InputComponent = ({ label, placeholder, name }: input) => {
+const InputComponent = ({ label, placeholder, name, initialValue }: input) => {
+    const [value, setValue] = useState(initialValue);
+
     return (
         <div className="flex flex-col gap-2 mb-6 align-center w-full">
             <Label htmlFor={name}>{label}</Label>
-            <Input placeholder={placeholder} name={name} type="text" />
+            <Input placeholder={placeholder} name={name} type="text" value={value} onChange={(event) => setValue(event.target.value)} />
         </div >
     )
 }
